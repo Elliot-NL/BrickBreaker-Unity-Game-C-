1f5a3b63ab21871e6c23f52c07749b745fdffb0f
Elliot-NL <elliotisvery@hotmail.co.uk>
2017-02-24 16:34:21 +0000

    Add files via upload

---
+++ BrickScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour
+{
+
+    public UIManager ui;
+
+    // Use this for initialization
+    void Start()
+    {
+        ui = GameObject.FindWithTag("UI").GetComponent<UIManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            ui.IncrementScore();
+            Destroy(gameObject);
+
+        }
+    }
+}
+
